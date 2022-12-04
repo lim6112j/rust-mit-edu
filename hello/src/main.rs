632557e4d5a73ed1f86fe464b489758316b368f5
ben lim <lim6112j@gmail.com>
2022-12-04 17:17:24 +0900

    request validation

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,5 +1,8 @@
 use std::fs::File;
-use std::{net::{TcpListener, TcpStream}, io::{Read, Write}};
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
@@ -9,16 +12,28 @@ fn main() {
         handle_connection(_stream);
     }
 }
-fn handle_connection(mut stream : TcpStream) {
+fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap(); 
+    stream.read(&mut buffer).unwrap();
     println!("Requested {}", String::from_utf8_lossy(&buffer[..]));
 
-    let mut file = File::open("home.html").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+    if buffer.starts_with(get) {
+        let mut file = File::open("home.html").unwrap();
 
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT Found\r\n\r\n";
+        let mut file = File::open("404.html").unwrap();
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).unwrap();
+        let response = format!("{}{}", status_line, contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
