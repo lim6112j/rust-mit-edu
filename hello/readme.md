963944b8583e27dda42634f05fa2db25ec66c3b9
ben lim <lim6112j@gmail.com>
2022-12-04 16:44:55 +0900

    initial commit

---
+++ hello/readme.md
@@ -0,0 +1,1 @@
+# [lecture](https://web.mit.edu/rust-lang_v1.25/arch/amd64_ubuntu1404/share/doc/rust/html/book/second-edition/ch20-01-single-threaded.html)
---
+++ hello/src/main.rs
@@ -0,0 +1,24 @@
+use std::fs::File;
+use std::{net::{TcpListener, TcpStream}, io::{Read, Write}};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let _stream = stream.unwrap();
+        handle_connection(_stream);
+    }
+}
+fn handle_connection(mut stream : TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap(); 
+    println!("Requested {}", String::from_utf8_lossy(&buffer[..]));
+
+    let mut file = File::open("home.html").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
