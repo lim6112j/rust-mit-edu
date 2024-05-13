f160e28524f4a43fbb99420e55a2a06973b9124a
byeong cheol lim <byeongcheollim@byeongui-MacBookPro.local>
2024-05-14 00:11:30 +0900

    *** empty log message ***

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+		let mut s1 = String::from("hello");
+		let _s2 = s1;
+		// intentional error, memory borrowed, shallow copy like but called as 'move'
+		// we can call this process 's1 moved to s2'
+		s1.push_str(", world!");
+		println!("{}",s1);
+		// Integer has known size at compile time and stored entirely on stack, works as if deep copy.
+		let x = 4;
+		let y = x;
+		println!("x={}, y={}", x, y);
 }
