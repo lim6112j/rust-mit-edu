2c3858c08e037c2e4c71eea0525f59d22833bb5d
byeong cheol lim <byeongcheollim@byeongui-MacBookPro.local>
2024-05-15 21:16:26 +0900

    hashmap

--- hashmap_example/src/main.rs
+++ hashmap_example/src/main.rs
@@ -3,5 +3,17 @@ fn main() {
 		let mut scores = HashMap::new();
 		scores.insert(String::from("blue"), 10);
 		scores.insert(String::from("red"), 20);
-    println!("Hello, world!");
+		let team_name = String::from("blue");
+		let score = scores.get(&team_name);
+    println!("Hello, world!, {}", score.unwrap());
+		for (key, value) in &scores {
+				println!("key:{}, value: {}", key, value);
+		}
+		let text = "hello world wonderful world";
+		let mut map = HashMap::new();
+		for word in text.split_whitespace() {
+				let count = map.entry(word).or_insert(0);
+				*count += 1;
+		}
+		println!("{:?}", map);
 }
